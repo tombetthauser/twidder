8f64391523b20bf1866b590f6e9b43636bfbb56e
tombetthauser <tombetthauser@gmail.com>
2020-02-09 17:59:05 -0800

    Adds gitignore and keys.js file along with mongoose

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+  
 const express = require("express");
 const app = express();
 
