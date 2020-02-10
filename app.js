6e3ab1b25db287f4fba58f99dc3bb5ef1ac59b37
tombetthauser <tombetthauser@gmail.com>
2020-02-09 17:41:56 -0800

    Adds and tests nodemon

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Hello There"));
 
 const port = process.env.PORT || 5000;
 
